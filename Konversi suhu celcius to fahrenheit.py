810543f8a2afd966fa5c58042125ed1fd4f1a1e6
Yahya Ahmad <106219715+yhyaahmd@users.noreply.github.com>
2023-09-03 16:30:17 +0700

    Tugas pelcod1

---
+++ Konversi suhu celcius to fahrenheit.py
@@ -0,0 +1,13 @@
+# Soal NIM ganjil nomor 1
+# Konversi suhu dari celcius ke fahrenheit
+# rumus celcius to fahrenheit 9/5*c+32
+
+print("Conversi suhu celcius to fahrenheit")
+celcius = int(input("Masukkan Suhu celcius :"))
+r1 = 9/5
+r2 = 32
+
+rumuscelciustofahrenheit = r1*celcius #rumus 9/5*c
+rumus2 = rumuscelciustofahrenheit+r2 #hasil rumus9/5*c lalu ditambah 32
+
+print("Maka suhu fahrenheitnya adalah", rumus2,"*f")
